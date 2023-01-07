066b5856aed008d4774bd9f2c526d9144bd2bf4e
Abhinav Sharma <abhinav291sharma@gmail.com>
2023-01-07 15:19:42 +0530

    Modified user Routes. Some routes are missing. Need to discuss

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,14 +1,17 @@
 const express = require('express')
-const {registerUser, loginUser, skillsUpdater, projectsUpdater, socialMediaHandlesUpdater, applyJobForUser} = require('../controllers/userControllers')
+const {registerUser, loginUser, skillsUpdater, projectsUpdater, socialMediaHandlesUpdater, applyJobForUser, getProjectsOfUser, postProjectsOfUser} = require('../controllers/userControllers')
 const {auth} = require("../middlewares/auth")
 const router = express.Router()
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.put("/skillsUpdate", auth ,skillsUpdater);
-router.put("/projectsUpdate", auth ,projectsUpdater);
+router.put("/projectsUpdate", auth ,projectsUpdater);   //Will have to work on this
 router.put("/socialMediaHandlesUpdate", auth ,socialMediaHandlesUpdater);
 router.get('/apply', applyJobForUser);
+
+router.get("/projectsGetterofUser", getProjectsOfUser);
+router.post("/projectSetterofUser", postProjectsOfUser);
 // router.post("/recruiter/regisrter", registerRecruiter);
 // router.post("/recruiter/login", loginRecruiter);
 // router.post("/recruiter/info", newRe)
