1ccf122aebd02b6ba54dd9136df5feb901541687
Preet Sojitra <preet.dev373@gmail.com>
2023-02-13 16:42:48 +0530

    fix: fixed the issue of cors in index file and add console.log in isLoggedin function in internalControllers
    added cors in index.js file and added console.log in isLoggedin function
    in internalControllers.js file to check whether the cookie is being sent
    in the request or not

--- controllers/internalControllers.js
+++ controllers/internalControllers.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 function isLoggedIn(req, res) {
+    // console.log(req.signedCookies["token"])  // This is to check whether the cookie is being sent or not
     try {
         if (req.signedCookies["token"] && jwt.verify(req.signedCookies["token"], process.env.JWT_SECRET)) return res.json({ isLoggedIn: true });
         return res.clearCookie("token").json({ isLoggedIn: false});
--- index.js
+++ index.js
@@ -1,62 +1,63 @@
-const express = require('express')
-require('dotenv').config();
-const cookieParser = require('cookie-parser')
-const fileUpload = require('express-fileupload'); 
-const morgan = require('morgan')
-const app = express();
-const mongoose = require('mongoose');
-const cors = require("cors");
-app.use(cors());
+const express = require("express")
+require("dotenv").config()
+const cookieParser = require("cookie-parser")
+const fileUpload = require("express-fileupload")
+const morgan = require("morgan")
+const app = express()
+const mongoose = require("mongoose")
+const cors = require("cors")
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true
+}))
 
 // const authenticateSession = require('./middleware/authenticateSession')
 
-
 // importing routes
-const user = require('./routes/userRoutes');
-const internalRoutes = require('./routes/internalRoutes');
-const openRoutes = require('./routes/openRoutes');
+const user = require("./routes/userRoutes")
+const internalRoutes = require("./routes/internalRoutes")
+const openRoutes = require("./routes/openRoutes")
 
 // regular middlewares
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 //cookie and file middlewares
-app.use(cookieParser(process.env.COOKIE_SECRET, { signed: true }))
+app.use(cookieParser(process.env.COOKIE_SECRET, {signed: true}))
 app.use(
-    fileUpload({
-        tempFileDir: "./tmp/",
-        useTempFiles: true,
-    })
-);
+  fileUpload({
+    tempFileDir: "./tmp/",
+    useTempFiles: true,
+  })
+)
 
 app.use((req, res, next) => {
-    res.header('Access-Control-ALlow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    // res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    next();
-});
+  // res.header("Access-Control-ALlow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  )
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000")
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next()
+})
 
 app.use((err, req, res, next) => {
-    console.log(err);
-    next();
-});
-
+  console.log(err)
+  next()
+})
 
 //logging
-app.use(morgan('tiny'));
-
-
+app.use(morgan("tiny"))
 
 // using routes
-app.use('/api/v1', user);
-app.use('/api/v1/internal', internalRoutes);
-app.use('/api/v1/profile', openRoutes);
-
+app.use("/api/v1", user)
+app.use("/api/v1/internal", internalRoutes)
+app.use("/api/v1/profile", openRoutes)
 
 // after all routes
-app.get((req,res)=>{
-    res.status(404).json({message: "Page not found"});
+app.get((req, res) => {
+  res.status(404).json({message: "Page not found"})
 })
 /*
     Have installed following additional dependencies:
@@ -67,20 +68,20 @@ app.get((req,res)=>{
     5) nodemon
 */
 
-
-mongoose.set('strictQuery', true)
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.set("strictQuery", true)
+mongoose.connect(
+  process.env.MONGO_URI,
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-},
-    (err) => {
-        if (err) {
-            console.log(err);
-        } else {
-            app.listen(process.env.PORT, () => {
-                console.log(`Server is running on port ${process.env.PORT}`);
-            });
-              
-        }
+  },
+  (err) => {
+    if (err) {
+      console.log(err)
+    } else {
+      app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`)
+      })
     }
-);
+  }
+)
